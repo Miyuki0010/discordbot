560a76893f0aec50d2120c9d1e08ec4fd454be93
Miyuki0010 <30321676+Miyuki0010@users.noreply.github.com>
2021-03-05 00:04:38 +0100

    Add files via upload

---
+++ DiscordBot/Commands/FunCommands.cs
@@ -0,0 +1,87 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscordBotTutorial.Bots.Commands
+{
+    public class FunCommands : BaseCommandModule
+    {
+        [Command("ping")]
+        [Description("Returns pong")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Pong").ConfigureAwait(false);
+        }
+
+        [Command("keanu")]
+        [Description("I will fuck it")]
+        public async Task Keanu(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("https://cdn.discordapp.com/attachments/767509294299873322/788827271103905804/unknown.png").ConfigureAwait(false);
+        }
+
+        [Command("add")]
+        [Description("Adds two numbers together")]
+        //[RequireRoles(RoleCheckMode.Any, "Moderator", "Owner")]
+        public async Task Add(CommandContext ctx,
+            [Description("First Number")] int numberOne,
+            [Description("Second Number")] int numberTwo)
+        {
+            await ctx.Channel
+                .SendMessageAsync((numberOne + numberTwo).ToString())
+                .ConfigureAwait(false);
+        }
+
+        [Command("respondmessage")]
+        public async Task RespondMessage(CommandContext ctx)
+        {
+            var interactivity = ctx.Client.GetInteractivity();
+
+            var message = await interactivity.WaitForMessageAsync(x => x.Channel == ctx.Channel).ConfigureAwait(false);
+
+            await ctx.Channel.SendMessageAsync(message.Result.Content);
+        }
+
+        [Command("respondreaction")]
+        public async Task RespondReaction(CommandContext ctx)
+        {
+            var interactivity = ctx.Client.GetInteractivity();
+
+            var message = await interactivity.WaitForReactionAsync(x => x.Channel == ctx.Channel).ConfigureAwait(false);
+
+            await ctx.Channel.SendMessageAsync(message.Result.Emoji);
+        }
+
+        [Command("poll")]
+        [Description("Work in Progress. Still buggy")]
+        public async Task Poll(CommandContext ctx, TimeSpan duration, params DiscordEmoji[] emojiOptions)
+        {
+            var interactivity = ctx.Client.GetInteractivity();
+            var options = emojiOptions.Select(x => x.ToString());
+
+            var pollEmbed = new DiscordEmbedBuilder
+            {
+                Title = "Poll",
+                Description = string.Join(" ", options)
+            };
+
+            var pollMessage = await ctx.Channel.SendMessageAsync(embed: pollEmbed).ConfigureAwait(false);
+
+            foreach (var option in emojiOptions)
+            {
+                await pollMessage.CreateReactionAsync(option).ConfigureAwait(false);
+            }
+
+            var result = await interactivity.CollectReactionsAsync(pollMessage, duration).ConfigureAwait(false);
+            var distinctResult = result.Distinct();
+
+            var results = distinctResult.Select(x => $"{x.Emoji}: {x.Total}");
+
+            await ctx.Channel.SendMessageAsync(string.Join("\n", results)).ConfigureAwait(false);
+        }
+    }
+}
---
+++ DiscordBot/Commands/ModCommands.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Threading.Tasks;
+
+namespace DiscordBotTutorial.Bots.Commands
+{
+    public class ModCommands : BaseCommandModule
+    {
+        [Command("kick")]
+        [Description("Warns")]
+        public async Task Kick(CommandContext ctx)
+        {
+            //await ctx.Channel
+        }
+
+    }
+}
